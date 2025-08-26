f1b0e8c1f46ff2f3dafb04177d7633467e12e0db
lblwstc <61739199+lblwstc@users.noreply.github.com>
2025-08-26 10:29:38 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GmailCleanupTool from './GmailCleanupTool';
+
+function App() {
+  return (
+    <div className="App">
+      <GmailCleanupTool />
+    </div>
+  );
+}
+
+export default App;
